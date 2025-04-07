1de7c52cdf243291c9f763d6ec17242552774f9c
Flooqey <132024466+Flooqey@users.noreply.github.com>
2025-04-07 10:26:09 -0300

    main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
 
+int main() {
     // Declaração das variáveis
     char estado1[3];
     char estado2[3];
@@ -23,8 +23,6 @@ int main(){
     float superPoder2;
     int pontosTur1;
     int pontosTur2;
-
-
 
     // Entrada de dados para a primeira cidade
     printf("Bem-vindo ao cadastrador de cartas Super Trunfo! Comece digitando uma letra de 'A' a 'H':\n");
@@ -72,25 +70,27 @@ int main(){
     printf("Por fim, digite a quantidade de pontos turísticos:\n");
     scanf("%d", &pontosTur2);
 
-   denspopulacional1 = populacao1 / area1;
+    // Cálculos
+    denspopulacional1 = populacao1 / area1;
     denspopulacional2 = populacao2 / area2;
     pibcapita1 = pib1 / populacao1;
     pibcapita2 = pib2 / populacao2;
     superPoder1 = populacao1 + area1 + pib1 + pontosTur1 + pibcapita1 + (1 / denspopulacional1);
     superPoder2 = populacao2 + area2 + pib2 + pontosTur2 + pibcapita2 + (1 / denspopulacional2);
 
-
     // Exibição dos dados cadastrados
     printf("\nÓtimo! Aqui estão as informações das cartas:\n");
+
     printf("\nCarta 1\n");
-    printf("Estado: %s\nCódigo: %s%s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\n Densidade Populacional: %.2f \n PIB per Capita: %.2f \n",
+    printf("Estado: %s\nCódigo: %s%s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f\n",
            estado1, estado1, codigo1, nomeCidade1, populacao1, area1, pib1, pontosTur1, denspopulacional1, pibcapita1);
 
     printf("\nCarta 2\n");
-    printf("Estado: %s\nCódigo: %s%s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\n Densidade Populacional: %.2f \n PIB per Capita: %.2f \n \n",
-        estado2, estado2, codigo2, nomeCidade2, populacao2, area2, pib2, pontosTur2, denspopulacional2, pibcapita2);
+    printf("Estado: %s\nCódigo: %s%s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões de reais\nPontos turísticos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f\n\n",
+           estado2, estado2, codigo2, nomeCidade2, populacao2, area2, pib2, pontosTur2, denspopulacional2, pibcapita2);
 
-       printf("\nComparação de Cartas:\n");
+    // Comparações
+    printf("\nComparação de Cartas:\n");
     printf("População: Vencedor - %s\n", (populacao1 > populacao2) ? nomeCidade1 : nomeCidade2);
     printf("Área: Vencedor - %s\n", (area1 > area2) ? nomeCidade1 : nomeCidade2);
     printf("PIB: Vencedor - %s\n", (pib1 > pib2) ? nomeCidade1 : nomeCidade2);
@@ -98,5 +98,6 @@ int main(){
     printf("Densidade Populacional: Vencedor - %s\n", (denspopulacional1 < denspopulacional2) ? nomeCidade1 : nomeCidade2);
     printf("PIB per Capita: Vencedor - %s\n", (pibcapita1 > pibcapita2) ? nomeCidade1 : nomeCidade2);
     printf("Super Poder: Vencedor - %s\n", (superPoder1 > superPoder2) ? nomeCidade1 : nomeCidade2);
+
     return 0;
 }
